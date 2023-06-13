c7383c760fdd3668056fe09017d6620f80e9775a
ovaisshaikh2 <shaikh.ovais@siardigital.com>
2023-06-13 12:59:42 +0500

    change new check

--- src/components/Fourth.jsx
+++ src/components/Fourth.jsx
@@ -5,7 +5,9 @@ import life from "../assets/Life at Image.png";
 
 const Fourth = (props) => {
   const navigate = useNavigate();
-
+  {
+    /* change */
+  }
   return (
     <React.Fragment>
       <section className="mb-8 xs:mb-10">
