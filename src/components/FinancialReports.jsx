ce144d4b3af02682a70f50412df4b83fbcdf7ee0
ovaisshaikh2 <shaikh.ovais@siardigital.com>
2023-06-14 15:56:32 +0500

    financial report

--- src/components/FinancialReports.jsx
+++ src/components/FinancialReports.jsx
@@ -12,7 +12,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2022
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Annual-Report-2022_compressed.pdf?v=1686312147"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -23,7 +23,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2021
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Annual-Report-2021.pdf?v=1683564398"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -34,7 +34,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2020
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Annual-Report-2020-TRPOL.pdf?v=1683564372"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -45,7 +45,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2019
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Annual-Report-2019.pdf?v=1683564338"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -56,7 +56,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2018
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Annual-Report-2018.pdf?v=1683564304"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -67,7 +67,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2017
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Annual_Report_2017.pdf?v=1683550654"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -78,7 +78,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Annual Reports 2016
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/June-2016-TPL.pdf?v=1683550615"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -96,7 +96,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Half Yearly 2022
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Half-Yearly-Report-December-2022-Final.pdf?v=1683549972"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -107,7 +107,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Half Yearly 2020
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Half_Year_2020.pdf?v=1683550007"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -118,7 +118,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Half Yearly 2019
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Half_Year_2019.pdf?v=1683550051"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -129,7 +129,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Half Yearly 2018
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/Half_Year_2018.pdf?v=1683550084"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -140,7 +140,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Half Yearly 2017
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/HalfYear2017.pdf?v=1683550107"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -151,7 +151,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Half Yearly 2016
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/HalfYear2016.pdf?v=1683550138"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -169,7 +169,7 @@ const FinancialReports = () => {
             <li className="py-1">
               First Quarter 2020
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/FQ20.pdf?v=1683549557"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -180,7 +180,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Third Quarter 2019
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/TQ19.pdf?v=1683549281"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -191,7 +191,7 @@ const FinancialReports = () => {
             <li className="py-1">
               First Quarter 2019
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/FQ19.pdf?v=1683549625"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -202,7 +202,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Third Quarter 2018
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/TQ18.pdf?v=1683549368"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -213,7 +213,7 @@ const FinancialReports = () => {
             <li className="py-1">
               First Quarter 2018
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/FQ18.pdf?v=1683549692"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -224,7 +224,7 @@ const FinancialReports = () => {
             <li className="py-1">
               Third Quarter 2017
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/TQ17.pdf?v=1683549440"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -235,7 +235,7 @@ const FinancialReports = () => {
             <li className="py-1">
               First Quarter 2017
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/FQ17.pdf?v=1683549734"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -246,7 +246,7 @@ const FinancialReports = () => {
             <li className="py-1">
               First Quarter 2016
               <a
-                className="text-black ml-4 text-xs hover:underline font-semibold"
+                className="text-black ml-4 text-sm hover:underline font-semibold"
                 href="https://cdn.shopify.com/s/files/1/0689/7349/4550/files/FQ16.pdf?v=1683549772"
                 target="_blank"
                 rel="noopener noreferrer"
