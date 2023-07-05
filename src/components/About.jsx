121d1347a6ddacfb8ff6da2433cb289c65f250aa
ovaisshaikh2 <shaikh.ovais@siardigital.com>
2023-07-05 10:31:06 +0500

    About revise content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,35 +20,37 @@ const About = () => {
         <div className="lg:col-span-2 justify-center text-[#919191] flex flex-col items-start lg:mr-32 xs:my-10 lg:mt-0 xs:mx-4 lg:mx-0">
           <h1 className="uppercase text-3xl mb-6 text-black">About Image</h1>
           <p className="">
-            Established in 1993, Image symbolises elegance, cementing its status
-            as one of Pakistan’s leading fashion and retail companies. From
-            being a brand that sells unstitched fabric to beautiful
-            ready-to-wear ensembles, Image has grown from strength to strength.
+            Established in 1993 as a fabric manufacturing company, Image
+            Pakistan Limited has grown to be one of Pakistan’s leading fashion
+            and retail houses. Image’s Schiffli embroidered fabric has been
+            renowned for its classic aesthetic and premium quality in the
+            textile field for decades. In a short span of a few years, Image has
+            established itself as a high-quality, fashion-forward clothing brand
+            with a store presence across five cities in Pakistan and a strong
+            global online presence; we ensure our product is accessible to women
+            worldwide.
           </p>
           <br></br>
           <p className="">
-            With a strong presence across 5 tier-A cities in Pakistan and online
-            global footprint, the brand has garnered widespread acclaim for its
-            classic aesthetic and premium quality. Furthermore, the brand
-            received the prestigious "Prime Ministers Excellence Award for
-            Outstanding Performance in E-commerce", as it became the first
-            seller on Amazon with a Pakistani ID.
+            To cater to the different needs of our customers, we offer both
+            Unstitched and Ready-to-Wear collections. We aim to create
+            affordable products that are minimal, chic, and timeless for women
+            across the globe. For our company’s outstanding performance in
+            e-commerce, including setting up the first shop on Amazon with a
+            Pakistan ID, we were awarded the “Prime Ministers Excellence Award”
+            at the National E-Commerce Convention in February 2022.
           </p>
           <br></br>
           <p className="">
-            To meet the growing demand of unstitched fabric that gives women the
-            freedom to stitch and style their outfits uniquely, the brand
-            launches premium unstitched Lawnkari, Winterkari and Printkari
-            collections respectively. With an unwavering commitment to elegance,
-            Image effortlessly combines fashion-forward trends at affordable
-            prices.
+            The quality of our fabric, stitching and chic yet timeless style has
+            made Image one of the most acclaimed household names today.
           </p>
-          <br></br>
+          {/* <br></br>
           <p className="">
             With our Ready-to-Wear and unstitched collections, we cater to women
             across the globe who are looking for fashionable and chic attires
             that is committed to unprecedented aesthetic combined with quality.
-          </p>
+          </p> */}
         </div>
       </div>
     </React.Fragment>
