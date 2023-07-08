22c77965a1a6e21521b51da3fb702b51ec5fbc2c
ovaisshaikh2 <shaikh.ovais@siardigital.com>
2023-07-08 13:00:39 +0500

    Fix more issue

--- src/components/MatrixPic.jsx
+++ src/components/MatrixPic.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
-import D1 from "../assets/matrix/1.JPG";
-import D2 from "../assets/matrix/2.JPG";
-import D3 from "../assets/matrix/3.JPG";
-import D4 from "../assets/matrix/4.JPG";
-import D5 from "../assets/matrix/5.JPG";
-import D6 from "../assets/matrix/6.JPG";
+import D1 from "../assets/matrix/4.jpg";
 
 const MatrixPic = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalImageSrc, setModalImageSrc] = useState("");
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   //update
-  const images = [D1, D2, D3, D4, D5, D6];
+  const images = [D1];
 
   const handleImageClick = (src, index) => {
     setModalImageSrc(src);
